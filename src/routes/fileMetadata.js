9cfdba5888d04bf5d8367529308370bb7f1df8e5
calebtuttle <1calebtuttle@gmail.com>
2022-05-15 11:14:56 -0400

    Comment out setFileMetadata

--- src/routes/fileMetadata.js
+++ src/routes/fileMetadata.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const fileMetadataService = require('../services/fileMetadata.service')
+const fileMetadataService = require("../services/fileMetadata.service");
 
-router.get('/', fileMetadataService.getFileMetadata)
-router.post('/', fileMetadataService.setFileMetadata)
-router.delete('/', fileMetadataService.deleteFileMetadata)
+router.get("/", fileMetadataService.getFileMetadata);
+// router.post('/', fileMetadataService.setFileMetadata)
+router.delete("/", fileMetadataService.deleteFileMetadata);
 
-module.exports = router
+module.exports = router;
--- src/services/fileMetadata.service.js
+++ src/services/fileMetadata.service.js
@@ -3,6 +3,27 @@ const axios = require("axios");
 const dbWrapper = require("../utils/dbWrapper");
 
 /**
+ * Get file metadata for every file belonging to the specified address.
+ * (Does not require authentication. Only modifications to a user's files require authentication.)
+ * Examples:
+ * curl -X GET http://localhost:3005/fileMetadata?address=0x0000000000000000000000000000000000000000
+ */
+const getFileMetadata = async (req) => {
+  console.log("getFileMetadata: Entered");
+  if (!req.query.address) {
+    return undefined;
+  }
+  const address = req.query.address.toLowerCase();
+  console.log(`address == ${address}`);
+  const files = await dbWrapper.getFilesByUserAddress(address);
+  if (files) {
+    console.log(`getFileMetadata: files.length == ${files.length}`);
+    return files;
+  }
+  console.log(`getFileMetadata: Address ${address} has no files`);
+};
+
+/**
  * Delete file by address && requestid.
  * Example:
  * curl -X DELETE http://localhost:3005/fileMetadata?address=address=0x0000000000000000000000000000000000000000&requestid=123
@@ -44,85 +65,40 @@ const deleteFileMetadata = async (req) => {
   return false;
 };
 
-const setFileMetadata = async (req) => {
-  console.log("setFileMetadata: Entered");
-  if (!req.body.address || !req.body.cid || !req.body.filename || !req.body.requestid) {
-    return undefined;
-  }
-  if (req.body.address.length != 42 || req.body.address.substring(0, 2) != "0x") {
-    return undefined;
-  }
-  const address = req.body.address.toLowerCase();
-  const cid = req.body.cid.toLowerCase();
-  const requestid = parseInt(req.body.requestid);
-  const filename = req.body.filename.toLowerCase();
+// TODO: Either delete this or only allow the filename to be changed. requestid should NOT be changed.
+// const setFileMetadata = async (req) => {
+//   console.log("setFileMetadata: Entered");
+//   if (!req.body.address || !req.body.cid || !req.body.filename || !req.body.requestid) {
+//     return undefined;
+//   }
+//   if (req.body.address.length != 42 || req.body.address.substring(0, 2) != "0x") {
+//     return undefined;
+//   }
+//   const address = req.body.address.toLowerCase();
+//   const cid = req.body.cid.toLowerCase();
+//   const requestid = parseInt(req.body.requestid);
+//   const filename = req.body.filename.toLowerCase();
 
-  const file = await dbWrapper.selectFile(["cid"], [cid]);
-  if (file) {
-    const columns = "address=?, filename=?, requestid=?";
-    const params = [address, filename, requestid, cid];
-    console.log(`setFileMetadata: Updating row in files: columns: ${columns} params: ${params}`);
-    dbWrapper.runSql(`UPDATE files SET ${columns} WHERE cid=?`, params);
-  } else {
-    const columns = "(address, filename, cid, requestid)";
-    const params = [address, filename, cid, requestid];
-    console.log(`setFileMetadata: Inserting row into files: columns: ${columns} params: ${params}`);
-    dbWrapper.runSql(`INSERT INTO files ${columns} VALUES (?, ?, ?, ?)`, params);
-  }
-  return true;
-};
-
-/**
- * Get file metadata for every file belonging to the specified address.
- * Examples:
- * curl -X GET http://localhost:3005/fileMetadata?address=0x0000000000000000000000000000000000000000
- */
-const getFileMetadata = async (req) => {
-  console.log("getFileMetadata: Entered");
-  if (!req.query.address) {
-    return undefined;
-  }
-  const address = req.query.address.toLowerCase();
-  console.log(`address == ${address}`);
-  const files = await dbWrapper.getFilesByUserAddress(address);
-  if (files) {
-    console.log(`getFileMetadata: files.length == ${files.length}`);
-    return files;
-  }
-  console.log(`getFileMetadata: Address ${address} has no files`);
-};
+//   const file = await dbWrapper.selectFile(["cid"], [cid]);
+//   if (file) {
+//     const columns = "address=?, filename=?, requestid=?";
+//     const params = [address, filename, requestid, cid];
+//     console.log(`setFileMetadata: Updating row in files: columns: ${columns} params: ${params}`);
+//     dbWrapper.runSql(`UPDATE files SET ${columns} WHERE cid=?`, params);
+//   } else {
+//     const columns = "(address, filename, cid, requestid)";
+//     const params = [address, filename, cid, requestid];
+//     console.log(`setFileMetadata: Inserting row into files: columns: ${columns} params: ${params}`);
+//     dbWrapper.runSql(`INSERT INTO files ${columns} VALUES (?, ?, ?, ?)`, params);
+//   }
+//   return true;
+// };
 
 module.exports = {
   getFileMetadata: async (req, res) => {
-    if (req.get("Authorization") != `Basic ${process.env.AUTH_TOKEN}`) {
-      return res.status(403).json({ error: "Incorrect Authorization header." });
-    }
     const files = await getFileMetadata(req);
-    if (files) {
-      return res.status(200).json(files);
-    }
-    return res.status(400).json({ error: "No such file" });
-  },
-  setFileMetadata: async (req, res) => {
-    if (req.get("Authorization") != `Basic ${process.env.AUTH_TOKEN}`) {
-      return res.status(403).json({ error: "Incorrect Authorization header." });
-    }
-    const newRow = {
-      address: req.body.address,
-      filename: req.body.filename,
-      cid: req.body.cid,
-      requestid: req.body.requestid,
-    };
-    const success = await setFileMetadata(req);
-    if (success) {
-      return res.status(200).json({
-        data: {
-          message: `Successfully set file metadata for file: ${req.body.filename}`,
-          newFileMetadata: newRow,
-        },
-      });
-    }
-    return res.status(400).json({ error: `An error ocurred trying to set file metadata for file: ${req.body.filename}` });
+    if (files) return res.status(200).json(files);
+    return res.status(400).json({ error: "No files for the specified address" });
   },
   deleteFileMetadata: async (req, res) => {
     if (req.get("Authorization") != `Basic ${process.env.AUTH_TOKEN}`) {
@@ -134,4 +110,25 @@ module.exports = {
     }
     return res.status(400).json({ error: `An error ocurred trying to set file metadata for file: ${req.body.filename}` });
   },
+  // setFileMetadata: async (req, res) => {
+  //   if (req.get("Authorization") != `Basic ${process.env.AUTH_TOKEN}`) {
+  //     return res.status(403).json({ error: "Incorrect Authorization header." });
+  //   }
+  //   const newRow = {
+  //     address: req.body.address,
+  //     filename: req.body.filename,
+  //     cid: req.body.cid,
+  //     requestid: req.body.requestid,
+  //   };
+  //   const success = await setFileMetadata(req);
+  //   if (success) {
+  //     return res.status(200).json({
+  //       data: {
+  //         message: `Successfully set file metadata for file: ${req.body.filename}`,
+  //         newFileMetadata: newRow,
+  //       },
+  //     });
+  //   }
+  //   return res.status(400).json({ error: `An error ocurred trying to set file metadata for file: ${req.body.filename}` });
+  // },
 };
