0f3db3d6a0f988c9428d1f182d49d110f8045ab6
calebtuttle <1calebtuttle@gmail.com>
2022-07-07 11:14:48 -0400

    Make logs more specific in estuaryWrapper

--- src/utils/estuaryWrapper.js
+++ src/utils/estuaryWrapper.js
@@ -21,7 +21,7 @@ module.exports.getPinsList = async () => {
     }));
     return pinMetadata;
   } catch (err) {
-    console.log(`Estuary API call: Error code: ${err.code}. Error message: ${err.message}`);
+    console.log(`estuaryWrapper.getPinsList: Error code: ${err.code}. Error message: ${err.message}`);
   }
   return undefined;
 };
@@ -41,7 +41,7 @@ module.exports.uploadFile = async (file, maxAttempts = 3) => {
       return resp.data;
     } catch (err) {
       numAttempts++;
-      console.log(`Estuary API call: Error code: ${err.code}. Error message: ${err.message}`);
+      console.log(`estuaryWrapper.uploadFile: Error code: ${err.code}. Error message: ${err.message}`);
     }
   }
 };
@@ -58,7 +58,7 @@ module.exports.deleteFile = async (requestid, maxAttempts = 3) => {
       return true;
     } catch (err) {
       numAttempts++;
-      console.log(`Estuary API call: Error code: ${err.code}. Error message: ${err.message}`);
+      console.log(`estuaryWrapper.deleteFile: Error code: ${err.code}. Error message: ${err.message}`);
     }
   }
 };
