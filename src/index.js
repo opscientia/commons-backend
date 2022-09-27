eaef055b59aaef7a45c856203017919132e13152
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2022-09-27 16:59:54 +0530

    create auth routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const passportSetup = require("./utils/passport-setup");
 
 const metadata = require("./routes/metadata");
 const uploadToEstuary = require("./routes/uploadToEstuary");
 const initializeUpload = require("./routes/initializeUpload");
+const orcidOauth = require("./routes/orcid-oauth");
+const req = require("express/lib/request");
 
 const app = express();
 
@@ -16,8 +19,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors(corsOptions));
 
+
 app.use("/metadata", metadata);
 app.use("/uploadToEstuary", uploadToEstuary);
 app.use("/initializeUpload", initializeUpload);
+app.use("/auth", orcidOauth);
 
 module.exports = app;
