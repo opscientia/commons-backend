fdb20ef89e23e31d6d860c2bf9096fbfc46ef43a
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2022-10-17 14:29:04 +0530

    progress

--- src/utils/passport-setup.js
+++ src/utils/passport-setup.js
@@ -17,31 +17,5 @@ passport.deserializeUser(function (id, done) {
 
 
 passport.use(
-  new OrcidStrategy(
-    {
-      sandbox: true, // remove this to use the production API
-      state: false, // remove this if not using sessions
-      clientID: process.env.ORCID_CLIENT_ID,
-      clientSecret: process.env.ORCID_CLIENT_SECRET,
-      callbackURL: "/auth/orcid/redirect",
-    },
-    (accessToken, refreshToken, params, profile, done) => {
-      // `profile` is empty as ORCID has no generic profile URL,
-      // so populate the profile object from the params instead
-      profile = { orcid: params.orcid, name: params.name };
-
-      dbAuthHandler.getUser({ orcid: profile.orcid }).then((currentUser) => {
-        if (currentUser) {
-          // User already exists, log their info
-          console.log("User is:" + currentUser);
-            done(null, currentUser);
-        } else {
-          dbAuthHandler.createUser(profile).then((newUser) => {
-            console.log("New User Created:" + newUser);
-            done(null, newUser);
-          });
-        }
-      });
-    }
-  )
+  
 );
